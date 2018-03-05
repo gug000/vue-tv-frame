b99688a55da3819ddf1b54a7bda99134fea6bdde
huang.qinghua <huang.qinghua@moretv.com.cn>
2018-03-05 18:58:54 +0800

    完成

---
+++ index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import FocVue from './components/FocClass'
+Vue.component(FocVue.name, FocVue);
+Vue.use(FocVue)
+'use strict'
+import './libs/Interface'
+import common from './libs/Common'
+import MtvCore from './libs/MtvCore'
+export {common,MtvCore}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+'use strict'
+const path = require('path')
+const webpack = require('webpack');
+function resolve (dir) {
+    return path.join(__dirname, '..', dir)
+}
+module.exports = {
+    entry: {
+        app: __dirname+'/libs/index.js'
+    },
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [path.resolve(__dirname, "./libs")],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    plugins: [
+
+    ]
+};
